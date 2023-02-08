8c6c31b4453ef4e98723250f07be7cd9e2e36ba9
Kosalvireak Khoeun <virakvary@gmail.com>
2023-02-08 16:00:44 +0700

    add partitiion

---
+++ PartitionOnly/QSBasic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartitionOnly
+{
+    internal class QSBasic
+    {
+        public int length;
+        int[] intArray = new int[15];
+
+        public void GenerateRandomIntegers( int length)
+        {
+            Random r = new Random();
+            for (int k = 0; k < length; k++)
+                intArray[k] = r.Next() % 100 + 1; 
+        }
+        public QSBasic(int length)
+        {
+            GenerateRandomIntegers(length);
+            this.length = length;
+        }
+        public void DisplayArray()
+        {
+            int length = intArray.Length;
+            Console.Write("Index: ");
+            for (int k = 0; k < length; k++)
+                Console.Write("{0,2}, ", k);
+            Console.WriteLine();
+            Console.Write("data:  ");
+            for (int k = 0; k < length; k++)
+                Console.Write("{0,2}, ", intArray[k]);
+            Console.WriteLine();
+        }
+        public void partitionIt(int lower, int higher)
+        {
+
+        }
+    }
+}
